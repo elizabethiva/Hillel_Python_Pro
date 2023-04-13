955a6f1460daa19fe6e785ca3c6b47ae65a4ad8a
Yelizaveta <lizavetamania@gmail.com>
2023-04-13 20:11:01 +0300

    update

--- lesson_3_Generators.py
+++ lesson_3_Generators.py
@@ -1,9 +1,10 @@
 from pympler import asizeof
 
 
-def find_in_file(file_name='rockyou.txt', pattern=input('Enter the search word: ')):
+def find_in_file(file_name='rockyou.txt', 
+                pattern=input('Enter the search word: ')):
     total_lines = 0
-    with open (file_name, 'r') as file:
+    with open(file_name, 'r') as file:
         with open('results.txt', 'w') as results:
             while True:
                 line = file.readline()
@@ -12,9 +13,9 @@ def find_in_file(file_name='rockyou.txt', pattern=input('Enter the search word: 
                 if pattern in line:
                     results.write(line)
                     total_lines += 1
-    return f"Total lines: {total_lines}. Total size:{asizeof.asized(results)}"
+    return f"Total lines: {total_lines}. Total size:{asizeof.asizeof(results)}"
 
-                        
+                     
 def main():
     res = find_in_file()
     print(res)
