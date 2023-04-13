11b1edadc3bc5168f9f054d5e79a21928e5b8ae7
Yelizaveta <lizavetamania@gmail.com>
2023-04-13 20:16:40 +0300

    code_quality

--- lesson_3_Generators.py
+++ lesson_3_Generators.py
@@ -1,7 +1,8 @@
 from pympler import asizeof
 
 
-def find_in_file(file_name="rockyou.txt", pattern=input("Enter the search word: ")):
+def find_in_file(file_name="rockyou.txt",
+                pattern=input("Enter the search word: ")):
     total_lines = 0
     with open(file_name, "r") as file:
         with open("results.txt", "w") as results:
