9590aad6808049416db4c6f1f81f4ad6a19009fb
Yelizaveta <lizavetamania@gmail.com>
2023-05-02 15:56:58 +0300

    done

--- lesson_5_Abstract classes.py
+++ lesson_5_Abstract classes.py
@@ -8,80 +8,97 @@ class SocialChannel:
         self.number_of_followers = number_of_followers
 
 
+class PostToChannel(ABC):
+    @abstractmethod
+    def post_to_channel(self):
+        """Post to the channel"""
+
+    @abstractmethod
+    def process_schedule(self, timestamp):
+        """This function should determine when the post will be published."""
+
+
 class Post:
     def __init__(self, message: str, timestamp: float):
         self.message = message
         self.timestamp = timestamp
 
 
-class PostToChannel(ABC):
-    @abstractmethod
-    def post_to_channel(self):
-        """Post to the channel"""
-
-
 class Twitter(PostToChannel):
-    def __init__(self, message, channel):
-        self.message = message
+    def __init__(self, channel, message):
         self.channel = channel
+        self.message = message
 
     def post_to_channel(self):
-        print(f"The post was published on {self.channel}: {self.message}")
+        print(f"The post was published on {self.channel}: '{self.message}'")
+
+    def process_schedule(self, timestamp):
+        while True:
+            if timestamp <= time.time():
+                self.post_to_channel()
+                break
 
 
 class Youtube(PostToChannel):
-    def __init__(self, message, channel):
-        self.message = message
+    def __init__(self, channel, message):
         self.channel = channel
+        self.message = message
 
     def post_to_channel(self):
-        print(f"The post was published on {self.channel}: {self.message}")
+        print(f"The post was published on {self.channel}: '{self.message}'")
+
+    def process_schedule(self, timestamp):
+        while True:
+            if timestamp <= time.time():
+                self.post_to_channel()
+                break
 
 
 class Facebook(PostToChannel):
-    def __init__(self, message, channel):
-        self.message = message
+    def __init__(self, channel, message):
         self.channel = channel
+        self.message = message
 
     def post_to_channel(self):
-        print(f"The post was published on {self.channel}: {self.message}")
+        print(f"The post was published on {self.channel}: '{self.message}'")
+
+    def process_schedule(self, timestamp):
+        while True:
+            if timestamp <= time.time():
+                self.post_to_channel()
+                break
 
 
-def post_a_message(channel: str, message: str) -> None:
-    post_to_twitter = Twitter(channel, message)
-    post_to_youtube = Youtube(channel, message)
-    post_to_facebook = Facebook(channel, message)
-    post_to_twitter.post_to_channel()
-    post_to_youtube.post_to_channel()
-    post_to_facebook.post_to_channel()
+def post_a_message(channel: str, message: str, timestamp: int) -> None:
+    if channel == "Twitter":
+        social_network = Twitter(channel, message)
+    if channel == "Youtube":
+        social_network = Youtube(channel, message)
+    if channel == "Facebook":
+        social_network = Facebook(channel, message)
+
+    social_network.process_schedule(timestamp)
 
 
-def process_schedule(posts: list, channels: list):
-    temp = posts.copy()
-    while len(temp):
-        for post in temp:
-            i = 0
-            while i < len(channels):
-                if time.time() >= post.timestamp:
-                    post_a_message(channels[i].type_of_channel, post.message)
-                    i += 1
-                if i == len(channels) - 1:
-                    temp.remove(post)
+def dispatcher(posts: list, channels: list):
+    for post in posts:
+        for channel in channels:
+            post_a_message(channel.type_of_channel, post.message, post.timestamp)
 
 
 def main():
     list_of_posts = [
-        Post(message='Hi guys!!', timestamp=1682866912.6884475),
-        Post(message='Subscribe!', timestamp=1682860160.410366),
-        Post(message='Bye!', timestamp=1682860100.410366)
+        Post(message='Hi guys!', timestamp=1683031500.6331794),
+        Post(message='Subscribe!', timestamp=1683031511.8375683),
+        Post(message='Bye!', timestamp=1683031524.2123072)
     ]
     list_of_social_channels = [
-        SocialChannel(type_of_channel="youtube", number_of_followers=100),
-        SocialChannel(type_of_channel="facebook", number_of_followers=10),
-        SocialChannel(type_of_channel="twitter", number_of_followers=1)
+        SocialChannel(type_of_channel="Twitter", number_of_followers=1),
+        SocialChannel(type_of_channel="Youtube", number_of_followers=100),
+        SocialChannel(type_of_channel="Facebook", number_of_followers=10)
     ]
 
-    process_schedule(list_of_posts, list_of_social_channels)
+    dispatcher(list_of_posts, list_of_social_channels)
 
 
 if __name__ == "__main__":
