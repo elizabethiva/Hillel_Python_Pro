f09513a663ed2cf5ba5795071adda27bdb39fdd8
Yelizaveta <lizavetamania@gmail.com>
2023-05-04 17:46:46 +0300

    code is improved

--- lesson_5_Abstract_classes.py
+++ lesson_5_Abstract_classes.py
@@ -1,104 +1,75 @@
 import time
 from abc import ABC, abstractmethod
+from dataclasses import dataclass
 
 
-class SocialChannel:
-    def __init__(self, type_of_channel: str, number_of_followers: int):
-        self.type_of_channel = type_of_channel
-        self.number_of_followers = number_of_followers
+@dataclass
+class SocialChannel(ABC):
+    type_of_channel: str
+    number_of_followers: int
 
-
-class Post:
-    def __init__(self, message: str, timestamp: float):
-        self.message = message
-        self.timestamp = timestamp
-
-
-class PostToChannel(ABC):
     @abstractmethod
-    def post_a_message(self):
+    def post_a_message(self, channel, message_to_post):
         """Post to the channel"""
 
-    @abstractmethod
-    def process_schedule(self, timestamp):
-        """This function should determine when the post will be published."""
+
+@dataclass
+class Post:
+    message: str
+    timestamp: int
 
 
-class Twitter(PostToChannel):
-    def __init__(self, channel, message):
-        self.channel = channel
-        self.message = message
+class Twitter(SocialChannel):
+    def __init__(self, number_of_followers: int):
+        self.type_of_channel = Twitter.__name__
+        self.number_of_followers = number_of_followers
 
-    def post_a_message(self):
-        print(f"The post was published on {self.channel}: '{self.message}'")
-
-    def process_schedule(self, timestamp):
-        while True:
-            if timestamp <= time.time():
-                self.post_a_message()
-                break
+    def post_a_message(self, message_to_post):
+        print(f"The post was published on {self.type_of_channel}: '{message_to_post}'")
 
 
-class Youtube(PostToChannel):
-    def __init__(self, channel, message):
-        self.channel = channel
-        self.message = message
+class Youtube(SocialChannel):
+    def __init__(self, number_of_followers: int):
+        self.type_of_channel = Youtube.__name__
+        self.number_of_followers = number_of_followers
 
-    def post_a_message(self):
-        print(f"The post was published on {self.channel}: '{self.message}'")
-
-    def process_schedule(self, timestamp):
-        while True:
-            if timestamp <= time.time():
-                self.post_a_message()
-                break
+    def post_a_message(self, message_to_post):
+        print(f"The post was published on {self.type_of_channel}: '{message_to_post}'")
 
 
-class Facebook(PostToChannel):
-    def __init__(self, channel, message):
-        self.channel = channel
-        self.message = message
+class Facebook(SocialChannel):
+    def __init__(self, number_of_followers: int):
+        self.type_of_channel = Facebook.__name__
+        self.number_of_followers = number_of_followers
 
-    def post_a_message(self):
-        print(f"The post was published on {self.channel}: '{self.message}'")
-
-    def process_schedule(self, timestamp):
-        while True:
-            if timestamp <= time.time():
-                self.post_a_message()
-                break
+    def post_a_message(self, message_to_post):
+        print(f"The post was published on {self.type_of_channel}: '{message_to_post}'")
 
 
-def instantiation(channel: str, message: str, timestamp: int) -> None:
-    if channel == "Twitter":
-        social_network = Twitter(channel, message)
-    if channel == "Youtube":
-        social_network = Youtube(channel, message)
-    if channel == "Facebook":
-        social_network = Facebook(channel, message)
-
-    social_network.process_schedule(timestamp)
-
-
-def dispatcher(posts: list, channels: list) -> None:
-    for post in posts:
-        for channel in channels:
-            instantiation(channel.type_of_channel, post.message, post.timestamp)
+def process_schedule(posts: list, social_channels: list):
+    """This function sorts the posts in ascending order of timestamp and calls the function post_a_message at the specified time."""
+    sorted_posts_list = sorted(posts, key=lambda post: post.timestamp)
+    for post in sorted_posts_list:
+        for channel in social_channels:
+            while True:
+                if post.timestamp <= time.time():
+                    channel.post_a_message(post.message)
+                    break
 
 
 def main():
     list_of_posts = [
-        Post(message="Hi guys!", timestamp=1683034464.4374373),
-        Post(message="Subscribe!", timestamp=1683034465.457672),
-        Post(message="Bye!", timestamp=1683034469.457672),
+        Post(message="Hi guys!", timestamp=1683210096.424286),
+        Post(message="Subscribe!", timestamp=1683210097.424286),
+        Post(message="Bye!", timestamp=1683210099.424286),
     ]
     list_of_social_channels = [
-        SocialChannel(type_of_channel="Twitter", number_of_followers=1000),
-        SocialChannel(type_of_channel="Youtube", number_of_followers=100),
-        SocialChannel(type_of_channel="Facebook", number_of_followers=10),
+        Twitter(number_of_followers=1000),
+        Youtube(number_of_followers=100),
+        Facebook(number_of_followers=10),
     ]
 
-    dispatcher(list_of_posts, list_of_social_channels)
+    process_schedule(list_of_posts, list_of_social_channels)
 
 
 if __name__ == "__main__":
