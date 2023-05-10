c37bd569a59718be25b765ac450b1aa639e3ae8c
Yelyzaveta Ivanova <118481379+elizabethiva@users.noreply.github.com>
2023-05-10 11:45:30 +0300

    Add files via upload

---
+++ lesson_9/Multiprocessing.py
@@ -0,0 +1,18 @@
+from time import perf_counter
+from multiprocessing import Process
+from IO_CPU_bound_tasks import encrypt_file, download_image
+
+
+if __name__ == "__main__":
+    try:
+        start = perf_counter()
+        process1 = Process(target=encrypt_file, args=("rockyou.txt",))
+        process2 = Process(target=download_image, args=("https://picsum.photos/1000/1000",))
+        process1.start()
+        process2.start()
+        process1.join()
+        process2.join()
+        total = perf_counter() - start
+        print(f"Total: {total} seconds")
+    except Exception as e:
+        print(f"Error occurred: {e}")
