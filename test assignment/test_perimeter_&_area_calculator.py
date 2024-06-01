88cae26552681b6d542928cb170c8b964a975c84
Yelizaveta <lizavetamania@gmail.com>
2024-06-01 15:19:52 +0300

    move files to folder

---
+++ test assignment/test_perimeter_&_area_calculator.py
@@ -0,0 +1,28 @@
+from perimeter_area_calculator import calculate_square, calculate_rectangle, calculate_circle
+
+
+def test_calculate_square():
+    figure, perimeter, area = calculate_square(2)
+    assert figure == 'Square'
+    assert perimeter == 8.0
+    assert area == 4.0
+    print("Square test passed")
+
+def test_calculate_rectangle():
+    figure, perimeter, area = calculate_rectangle(2, 2, 1, 1)
+    assert figure == 'Rectangle'
+    assert perimeter == 4.0
+    assert area == 1.0
+    print("Rectangle test passed")
+
+def test_calculate_circle():
+    figure, perimeter, area = calculate_circle(2)
+    assert figure == 'Circle'
+    assert round(perimeter, 2) == 12.57
+    assert round(area, 2) == 12.57
+    print("Circle test passed")
+
+
+test_calculate_square()
+test_calculate_rectangle()
+test_calculate_circle()
